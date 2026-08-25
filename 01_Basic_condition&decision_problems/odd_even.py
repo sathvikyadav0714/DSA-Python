5058898701d88565b83ea45c49c8538b7f76a864
sathvikyadav0714 <sathvikyadav0714@gmail.com>
2026-08-26 00:30:23 +0530

    Initial commit

---
+++ 01_Basic_condition&decision_problems/odd_even.py
@@ -0,0 +1,11 @@
+n=int(input("enter a number : "))
+# if n%2==0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+
+if n&1==0:
+    print("even number")
+else:
+    print("odd number")
