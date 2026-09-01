2724d41c06a3ffa4f6b64f1427d8d34b4e61658b
sathvikyadav0714 <sathvikyadav0714@gmail.com>
2026-09-02 00:05:47 +0530

    strong number

---
+++ 02_Number_Problems/N02_strong_number.py
@@ -0,0 +1,19 @@
+# Given an integer n, determine whether it is a Strong number.
+
+# A number is Strong if the sum of the factorials of its digits equals the original number.
+
+n=145
+temp=n
+total_sum=0
+while n>0:
+    last_digit=n%10
+    
+    factorial=1
+    for i in range(1,last_digit+1):
+        factorial*=i
+    total_sum+=factorial
+    n=n//10
+if total_sum==temp:
+    print(f"{temp} is a Strong number.")
+else:   
+    print(f"{temp} is not a Strong number.")
